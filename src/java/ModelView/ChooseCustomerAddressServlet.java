7eddd496f371cc95e6a1d8aa80454ddf9f98ed67
twoports <dodd.gavin@gmail.com>
2014-01-14 19:40:36 +0000

    With added HTML form and servlet page examples.

--- src/java/ModelView/ChooseCustomerAddressServlet.java
+++ src/java/ModelView/ChooseCustomerAddressServlet.java
@@ -4,14 +4,13 @@
  * and open the template in the editor.
  */
 
-package foodclubservlets;
+package ModelView;
 
 ;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,8 +21,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 
 
-@WebServlet(name = "ChooseClubServlet", urlPatterns = {"/ChooseClubServlet"})
-public class ChooseClubServlet extends HttpServlet {
+public class ChooseCustomerAddressServlet extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -39,15 +37,14 @@ public class ChooseClubServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         try {
-            
+            /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet ChooseClubServlet</title>");            
+            out.println("<title>Servlet ChooseCustomerAddressServlet</title>");            
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet ChooseClubServlet at " + request.getContextPath() + "</h1>");
-            out.println("Hello " + request.getParameter("club") + " , are you ready to order?");
+            out.println("<h1>Servlet ChooseCustomerAddressServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         } finally {
